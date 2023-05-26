1f57d5be837f74c829e651e50ae6c986eaa7f429
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-05-26 00:26:48 -0700

    this_strtok.c

---
+++ this_strtok.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+*this_strtok - this function that gets token from a string
+*@sf: pointer to the address of string
+*@tk: pointer to the delimiter
+*Return: pointer ro the address of next token or NULL if none
+*/
+
+char *this_strtok(char *sf, constant *tk)
+{
+	static char *o, *c;
+	unsigned int i;
+
+	if (sf != NULL)
+		c = sf;
+	o = c;
+	if (o == NULL)
+		return (NULL);
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (check_delim(c[i], tk) == 1)
+			break;
+	}
+	if (c[i] == '\0')
+		c = NULL;
+	else
+	{
+		c[i] = '\0';
+		c = c + i + 1;
+
+		if (*c == '\0')
+			c = NULL;
+	}
+	return (o);
+}
